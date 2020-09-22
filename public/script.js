2c3c61c275b0d68a8cb5efceb721196ed2929410
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-22 17:49:35 +0300

    refactore fetch api

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 
+
+
 const form = document.getElementById('convertForm');
 const query = document.getElementById('input_query');
 const target = document.getElementById('api_result');
@@ -6,18 +8,14 @@ const target = document.getElementById('api_result');
 form.addEventListener('submit', e => {
 
     e.preventDefault();
-    // console.log(query.value);
+
     fetch(`/api/convert?input=${query.value}` )
     .then(res => res.json())
     .then( data => {
-        // console.log(data)
-        if ( data.error ) {
-            target.innerText = JSON.stringify(data);
-        } else {
-            let str = JSON.stringify(data.string);
-            str = str.replace('"', '').replace('"', '')
-            target.innerText = str;
-        }
+
+        let str = JSON.stringify(data.string || data);
+        str = str.replace('"', '').replace('"', '')
+        target.innerText = str;
         
     })
 })
