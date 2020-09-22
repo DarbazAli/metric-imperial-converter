1b4e552fbd526d5de83efba40ed50114d86514e9
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-22 16:26:09 +0300

    create api route

---
+++ server/api.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Handling API Route
+const ConvertHandler = require('./convertHandler');
+
+module.exports = app => {
+    const convertHandler = new ConvertHandler();
+
+    const { getNum, getUnit, convert, getReturnUnit, toString} = convertHandler();
+
+    app
+    .route('/api/convert')
+    .get( (req, res) => {
+        const {input} = req.query;
+
+        const inintNum = getNum(input)
+        const initUnit = getUnit(input)
+        const returnNum = convert(inintNum, initUnit)
+        const returnUnit = getReturnUnit(initUnit)
+        const string = toString(inintNum, initUnit, returnNum, returnUnit)
+
+        let response = {};
+
+        if ( !input ) {
+            response.error = 'invalid number and unit'
+        }
+        else if ( inintNum && !initUnit ) {
+            response.error = 'invalid unit'
+        }
+        else {
+            response = {
+                inintNum,
+                initUnit,
+                returnNum,
+                returnUnit,
+                string
+            }
+        }
+
+        // respond back with json object
+        res.json(response)
+    })
+
+
+
+}
